db142e9fd573424e008a37eea7129554a23f0745
sum1991 <ls.sumukh@gmail.com>
2020-01-23 20:32:31 -0700

    first commit

---
+++ src/LeetCode/ArrayPartition.java
@@ -0,0 +1,15 @@
+package LeetCode;
+import java.util.Arrays;
+
+public class ArrayPartition {
+
+
+
+    public int arrpairsum(int nums[]){
+        Arrays.sort(nums);
+        for (int num : nums){
+            System.out.println(num);
+        }
+        return 1;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,9 @@
+import LeetCode.*;
+public class main {
+    public static void main(String[] args) {
+        System.out.println("print the value");
+        ArrayPartition arrpair= new ArrayPartition();
+        int[] nums ={1,4,3,2};
+        arrpair.arrpairsum(nums);
+    }
+}
