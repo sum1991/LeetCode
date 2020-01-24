6aca32605a7634283bdf0cbe61aa0a42356dde14
sum1991 <ls.sumukh@gmail.com>
2020-01-23 21:20:02 -0700

    fix the array partion

--- src/LeetCode/ArrayPartition.java
+++ src/LeetCode/ArrayPartition.java
@@ -7,9 +7,13 @@ public class ArrayPartition {
 
     public int arrpairsum(int nums[]){
         Arrays.sort(nums);
-        for (int num : nums){
-            System.out.println(num);
+        int groupBy = 2;
+        int total=0;
+        for(int j=0;j<nums.length;j=(j+groupBy)){
+            if(j+1==nums.length)
+                break;
+            total =total + nums[j];
         }
-        return 1;
+        return total;
     }
 }
--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("print the value");
         ArrayPartition arrpair= new ArrayPartition();
-        int[] nums ={1,4,3,2};
-        arrpair.arrpairsum(nums);
+        int[] nums ={7,3,1,0,0,6};
+        System.out.println(arrpair.arrpairsum(nums));
     }
 }
