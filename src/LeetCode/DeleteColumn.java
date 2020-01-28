3e4c519c884d4a3967097ab26fa00c438327b98f
sum1991 <ls.sumukh@gmail.com>
2020-01-27 23:16:54 -0700

    add column

---
+++ src/LeetCode/DeleteColumn.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+import java.util.ArrayList;
+
+public class DeleteColumn {
+
+    public int minDeletionSize(String[] A) {
+        int countChar=0;
+        ArrayList<Integer> index = new ArrayList<>();
+        boolean AddIndex=true;
+        while(countChar<=A[0].length()-1){
+            AddIndex = true;
+            for(int i=0;i>(A.length-1);i++){
+                if(A[i].charAt(countChar)<A[i+1].charAt(countChar)){
+                    index.add(countChar);
+                    break;
+                }
+            }
+            countChar++;
+        }
+
+        return index.size();
+    }
+}
--- src/main.java
+++ src/main.java
@@ -9,7 +9,7 @@ public class main {
         //DIString di = new DIString();
         //System.out.println(di.diStringMatch(s));
 
-        RecentCounter obj = new RecentCounter();
+        /*RecentCounter obj = new RecentCounter();
         int param_1 = obj.ping(1);
         System.out.println(param_1);
         int param_2 = obj.ping(100);
@@ -17,6 +17,11 @@ public class main {
         int param_3 = obj.ping(3001);
         System.out.println(param_3);
         int param_4 = obj.ping(3002);
-        System.out.println(param_4);
+        System.out.println(param_4);*/
+
+        String[] s ={"cba","daf","ghi"};
+        DeleteColumn dc = new DeleteColumn();
+        dc.minDeletionSize(s);
+        System.out.println(dc.minDeletionSize(s));
     }
 }
